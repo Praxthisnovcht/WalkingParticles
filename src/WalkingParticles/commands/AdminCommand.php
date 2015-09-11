35ec5a646ec544d651e8912e3015755bc22b0075
Mak Ho Yin <hoyinm.dev@gmail.com>
2015-09-11 20:17:21 +0800

    It should be fixed..

--- src/WalkingParticles/commands/AdminCommand.php
+++ src/WalkingParticles/commands/AdminCommand.php
@@ -405,8 +405,7 @@ class AdminCommand extends BaseCommand{
 									$target = $this->getPlugin()->getServer()->getPlayer($args[1]);
 									if($target !== null){
 										$this->getPlugin()->tryPlayerParticle($issuer, $target);
-										$particles = new Particles($this->getPlugin());
-										$issuer->sendMessage($this->getPlugin()->colourMessage("&aYou have &e10 &aseconds to test &b" . $target->getName() . "&a's particles!\n&aParticles which " . $target->getName() . " using: &6" . $particles->getAll($target)));
+										$issuer->sendMessage($this->getPlugin()->colourMessage("&aYou have &e10 &aseconds to test &b" . $target->getName() . "&a's particles!\n&aParticles which " . $target->getName() . " using: &6" . $this->getPlugin()->getPlayerParticles($target)));
 										return true;
 									} else{
 										$issuer->sendMessage($this->plugin->colourMessage("&cInvalid target!"));
